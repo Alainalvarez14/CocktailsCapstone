b46ec705671f972a329132e22802a225e612d465
Alain Alvarez <alainalvarez14@gmail.com>
2023-01-30 16:37:55 -0500

    added searchbar component and functionality, can properly search for cocktails

---
+++ frontend/src/components/Searchbar/Searchbar.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import SpecificCocktail from "../SpecificCocktail/SpecificCocktail";
+import { useHistory } from "react-router-dom";
+
+const Searchbar = () => {
+
+    const dispatch = useDispatch();
+    const [name, setName] = useState('');
+    const allCocktails = useSelector(state => state.cocktails);
+    const [searchResults, setSearchResults] = useState('');
+    const history = useHistory();
+
+    useEffect(() => {
+        if (allCocktails) setSearchResults(Object.values(allCocktails).filter(cocktail => cocktail.name.toLowerCase().includes(name.toLowerCase())));
+    }, [name]);
+
+    const searchCocktail = (e) => {
+        e.preventDefault()
+        // setSearchResults(Object.values(allCocktails).filter(cocktail => cocktail.name.toLowerCase().includes(name.toLowerCase())));
+        // setName('');
+    }
+
+    const openSpecificCocktail = (e, cocktail) => {
+        e.preventDefault();
+        setName('');
+        setSearchResults('');
+        history.push(`/drink/${cocktail.id}`);
+    }
+
+    return (
+        <div>
+            <form class="d-flex" role="search" onSubmit={(e) => searchCocktail(e)}>
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={name} onChange={(e) => setName(e.target.value)}></input>
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            {searchResults && name && (
+                <div>{searchResults.map(cocktail => {
+                    return (
+                        <div onClick={(e) => openSpecificCocktail(e, cocktail)}>{cocktail.name}</div>
+                    )
+                })}</div>
+            )}
+        </div>
+    )
+}
+
+export default Searchbar;
