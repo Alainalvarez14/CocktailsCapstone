5b94cb1ad7263f32f2c00c8d2da9cf733e660975
Alain Alvarez <alainalvarez14@gmail.com>
2023-02-07 16:14:17 -0500

    attempting to seed on render

--- backend/db/models/cocktail.js
+++ backend/db/models/cocktail.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
+     *
      */
     static associate(models) {
       Cocktail.hasMany(models.Review, { onDelete: 'CASCADE', foreignKey: 'cocktailId' });
