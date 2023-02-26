96afcd83b4e5e468d6adff12fea5fd4594f07f4d
Alain Alvarez <alainalvarez14@gmail.com>
2023-02-25 23:56:00 -0500

    fixed edit user modal closing upon submission if errors exist, display errors when exist

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -22,6 +22,26 @@ const validateLogin = [
     handleValidationErrors
 ];
 
+const validateEditUser = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
 // Log in
 router.post(
     '/',
