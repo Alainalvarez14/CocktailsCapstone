56d98a0f8e8425016ed6e796703ab4014e1ae9f6
Alain Alvarez <alainalvarez14@gmail.com>
2023-03-04 14:27:06 -0500

    added media queries for signup/login modals

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,22 +1,15 @@
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import * as sessionActions from "../../store/session";
-// import { Redirect } from 'react-router-dom';
 import { useModal } from "../../context/Modal";
-// import "./LoginForm.css"
 
 const LoginFormModal = () => {
 
     const dispatch = useDispatch();
-    // const sessionUser = useSelector(state => state.session.user);
     const [credential, setCredential] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState([]);
     const { closeModal } = useModal();
-
-    // if (sessionUser) return (
-    //     <Redirect to="/" />
-    // );
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -47,30 +40,7 @@ const LoginFormModal = () => {
                 justifyContent: 'center',
                 marginBottom: '2vh',
             }}>Log In</h1>
-            {/* <form onSubmit={handleSubmit}>
-                <ul>
-                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-                </ul>
-                <label>
-                    Username or Email
-                    <input
-                        type="text"
-                        value={credential}
-                        onChange={(e) => setCredential(e.target.value)}
-                        required
-                    />
-                </label>
-                <label>
-                    Password
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit">Log In</button>
-            </form> */}
+
             <form onSubmit={handleSubmit}>
                 <ul>
                     {errors.map((error, idx) => <li key={idx}>{error}</li>)}
